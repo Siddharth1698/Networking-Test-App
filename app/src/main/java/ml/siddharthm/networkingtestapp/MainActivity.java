95ac6be2d5e93bcfbd8c5e9988d5842068a15b5e
Siddharth M <siddharth.m98@gmail.com>
2018-10-28 12:40:18 +0530

    Adding Retrofit and get some info

--- app/src/main/java/ml/siddharthm/networkingtestapp/MainActivity.java
+++ app/src/main/java/ml/siddharthm/networkingtestapp/MainActivity.java
@@ -2,6 +2,16 @@ package ml.siddharthm.networkingtestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.ipify.org/").build();
+        Api api = retrofit.create(Api.class);
+        api.getIp().enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.i("Retrofit test app",response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
+    interface Api{
+
+        @GET("/")
+        Call<ResponseBody> getIp();
     }
 }
