686f4473f3c49768c36d349c6d3991628acc0802
Siddharth M <siddharth.m98@gmail.com>
2018-10-28 19:53:24 +0530

    POST Multipart file

--- app/src/main/java/ml/siddharthm/networkingtestapp/MainActivity.java
+++ app/src/main/java/ml/siddharthm/networkingtestapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -28,24 +31,35 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/").build();
         Api api = retrofit.create(Api.class);
 
-        Map<String,String> params = new HashMap<>();
-        params.put("userId","1");
-        params.put("id","2");
-         api.getPById(1).enqueue(new Callback<ResponseBody>() {
-             @Override
-             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                 try {
-                     Log.d("retro",response.body().string());
-                 } catch (IOException e) {
-                     e.printStackTrace();
-                 }
-             }
 
-             @Override
-             public void onFailure(Call<ResponseBody> call, Throwable t) {
+        try {
+            File file = new File(getCacheDir(),"hello.txt");
+            FileWriter writer = new FileWriter(file);
+            writer.append("hello");
+            writer.flush();
+            writer.close();
 
-             }
-         });
+            RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            MultipartBody.Part part = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
+
+            api.uploadFile("https://file.io/", part).enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    try {
+                        Log.d("RetrofitTag", response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
